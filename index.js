2e8838f13fa24d792f6836ab4f33883fbedffa7c
raymondborkowski <rborkows@umich.edu>
2018-02-06 17:44:47 -0500

    RB: Working on adding different overrides

--- index.js
+++ index.js
@@ -1,54 +1,43 @@
-var defaultOptions = {
-    firstDayOfWeek: 0,
-    months: {
-        long: ['January','February','March','April','May','June','July','August','September','October','November','December'],
-    },
-    weekdays: {
-        short: ['S','M','T','W','T','F','S'],
-    },
-};
-var MONTH_NAV = 'mn';
-var DIV = 'div';
-
-function extend(out) {
-    out = out || {};
-    for (var i = 1; i < arguments.length; i++) {
-        if (arguments[i]) {
-            for (var key in arguments[i]) {
-                if (arguments[i].hasOwnProperty(key)) {
-                    out[key] = arguments[i][key];
-                }
-            }
-        }
-    }
-    return out;
-};
-
-function parseDate(input) {
-    var parts = input.split('-');
-    return new Date(parts[0], parts[1]-1, parts[2]);
-}
-
 function TinyPicker(query, options) {
+    var defaultOptions = {
+        start: 1,
+        format: '%Y-%m-%d',
+        months: ['January','February','March','April','May','June','July','August','September','October','November','December'],
+        days: ['S','M','T','W','T','F','S'],
+        doubleMonth: true,
+        stayOpen: true,
+        range: true,
+        history: false
+    };
+    var DIV = 'div';
+    var queryElms;
+    var currentDate = new Date();
+
     if (!(this instanceof TinyPicker)) {
         return new TinyPicker(query, options);
     }
 
     options = extend({}, defaultOptions, options);
-    window.document.addEventListener('click', function(e) {setOpenBindings(e);}, false);
+    document.addEventListener('click', function(e) {setOpenBindings(e.target);}, false);
     var picker = null, targetEl = null, current, selected;
 
-    function setOpenBindings(e) {
-        var target = e.target;
-        if (target.className === MONTH_NAV+ ' next') {
+    function setOpenBindings(target) {
+        queryElms = document.querySelectorAll(query);
+        var className = target.className;
+        if (className === 'next') {
             getNextMonth();
-        } else if (target.className ===  MONTH_NAV+ ' prev') {
+        } else if (className === 'prev') {
             getPreviousMonth();
-        } else if (target.className === 'day') {
+        } else if (className === 'day') {
             setDate(target.innerHTML);
-            close();
+            if (options.stayOpen) {
+                close();
+                openNext();
+            } else {
+                close();
+            }
         } else {
-            while (target && !matchesReferrerEl(target) && target.className !== 'dp') {
+            while (target && !matchesReferrerEl(target) && className !== 'dp') {
                 target = target.parentNode;
             }
             if (target && matchesReferrerEl(target)) { render(target); }
@@ -56,19 +45,24 @@ function TinyPicker(query, options) {
         }
     }
 
+    function openNext() {
+        for (var i = 0; i < queryElms.length; i++) {
+            if (!queryElms[i].value) {render(queryElms[i]);}
+        }
+    }
+
     function matchesReferrerEl(elm) {
-        var referrers = document.querySelectorAll(query);
-        for (var i = 0; i < referrers.length; i++) {
-            if (elm === referrers[i]) return true;
+        for (var i = 0; i < queryElms.length; i++) {
+            if (elm === queryElms[i]) {return true;}
         }
         return false;
     }
 
     function addWeek(days) {
-        var week = document.createElement(DIV);
+        var week = createEl(DIV);
         week.classList.add('w');
         for (var i = 0; i < days.length; i++) {
-            week.appendChild(days[i]);
+            appendChild(week, days[i]);
         }
         return week;
     }
@@ -76,7 +70,6 @@ function TinyPicker(query, options) {
     function render(target) {
         targetEl = typeof target !== typeof undefined ? target : targetEl;
         if (target || typeof current === typeof undefined) {
-            var currentDate = new Date();
             if (target) { selected = null; }
             if (target && target.value) {
                 currentDate = parseDate(target.value.toLowerCase());
@@ -101,90 +94,84 @@ function TinyPicker(query, options) {
     }
 
     function draw() {
-        var position = {
-            x: targetEl.offsetLeft,
-            y: targetEl.offsetTop + targetEl.offsetHeight,
-        };
-        picker = document.createElement(DIV);
+        picker = createEl(DIV);
         picker.classList.add('dp');
-        picker.setAttribute('style', 'left:' + position.x + 'px;top:' + position.y + 'px;solid:blue;');
-        picker.appendChild(getNav());
-        picker.appendChild(getHeader());
-        var weeks = getWeeks();
-        for (var i = 0; i < weeks.length; i++) {
-            picker.appendChild(weeks[i]);
+        picker.style.cssText = 'left:' + targetEl.offsetLeft+ 'px;top:' + (targetEl.offsetTop + targetEl.offsetHeight) + 'px;solid:blue;';
+        appendChild(picker, getNav());
+        appendChild(picker, getHeader());
+        for (var i = 0; i < getWeeks().length; i++) {
+            appendChild(picker, getWeeks()[i]);
         }
 
         targetEl.parentNode.insertBefore(picker, targetEl.nextSibling);
     }
 
     function getNav() {
-        var nav = document.createElement(DIV);
+        var nav = createEl(DIV);
         nav.classList.add('tn');
 
-        var previousMonth = document.createElement(DIV);
-        previousMonth.classList.add(MONTH_NAV);
-        previousMonth.classList.add('prev');
+        var previousMonth = createEl(DIV);
+        previousMonth.className = 'prev';
         previousMonth.innerHTML = '<';
 
-        var currentMonth = document.createTextNode(options.months.long[current.month] + ' ' + current.year);
+        var currentMonth = document.createTextNode(options.months[current.month] + ' ' + current.year);
 
-        var nextMonth = document.createElement(DIV);
-        nextMonth.classList.add(MONTH_NAV);
-        nextMonth.classList.add('next');
+        var nextMonth = createEl(DIV);
+        nextMonth.className = 'next';
         nextMonth.innerHTML = '>';
 
-        nav.appendChild(previousMonth);
-        nav.appendChild(currentMonth);
-        nav.appendChild(nextMonth);
+        appendChild(nav, previousMonth);
+        appendChild(nav, currentMonth);
+        appendChild(nav, nextMonth);
 
         return nav;
     }
 
     function getHeader() {
-        var weekdays = options.weekdays.short.slice(options.firstDayOfWeek).concat(options.weekdays.short.slice(0, options.firstDayOfWeek));
-        var header = document.createElement(DIV);
+        var weekdays = options.days.slice(options.start).concat(options.days.slice(0, options.start));
+        var header = createEl(DIV);
         header.classList.add('wh');
         for (var i = 0; i < 7; i++) {
-            var dayOfWeek = document.createElement(DIV);
+            var dayOfWeek = createEl(DIV);
             dayOfWeek.innerHTML = weekdays[i];
-            header.appendChild(dayOfWeek);
+            appendChild(header, dayOfWeek);
         }
         return header;
     }
 
     function getWeeks() {
-        // Get first day of month and update acconding to options
         var firstOfMonth = new Date(current.year, current.month, 1).getDay();
-        firstOfMonth = firstOfMonth < options.firstDayOfWeek ? 7 + (firstOfMonth - options.firstDayOfWeek) : firstOfMonth - options.firstDayOfWeek;
+        firstOfMonth = firstOfMonth < options.start ? 7+(firstOfMonth - options.start ) : firstOfMonth - options.start;
         var daysInMonth = new Date(current.year, current.month + 1, 0).getDate();
         var days = [];
         var weeks = [];
 
         // Create empty spaces for last month
         for (var i = firstOfMonth - 1; i >= 0; i--) {
-            days.push(document.createElement(DIV));
+            days.push(createEl(DIV));
         }
-        // Define days in current month
+        // current month days
         for (var i = 0; i < daysInMonth; i++) {
             if (i && (firstOfMonth + i) % 7 === 0) {
                 weeks.push(addWeek(days));
                 days = [];
             }
-            var currentMonthDay = document.createElement(DIV);
+            var currentMonthDay = createEl(DIV);
             currentMonthDay.classList.add('day');
-            if (selected && selected.year === current.year &&
-                selected.month === current.month && selected.day === i + 1) {
+            if (selected && selected.year === current.year && selected.month === current.month && selected.day === i + 1) {
                 currentMonthDay.classList.add('selected');
             }
             currentMonthDay.innerHTML = i + 1;
+            if (currentDate.getDate() > currentMonthDay.innerHTML && currentDate.getMonth() === current.month && currentDate.getFullYear() === current.year) {
+                currentMonthDay.classList.add('disabled');
+            }
             days.push(currentMonthDay);
         }
         // Create empty spaces for next month
         if (days.length) {
             var len = days.length;
             for (var i = 0; i < 7 - len; i++) {
-                days.push(document.createElement(DIV));
+                days.push(createEl(DIV));
             }
             weeks.push(addWeek(days));
         }
@@ -192,26 +179,27 @@ function TinyPicker(query, options) {
     }
 
     function getNextMonth() {
-        var currentDate = new Date(current.year, current.month + 1);
+        var currentSelectedDate = new Date(current.year, current.month + 1);
         current = {
-            year: currentDate.getFullYear(),
-            month: currentDate.getMonth(),
+            year: currentSelectedDate.getFullYear(),
+            month: currentSelectedDate.getMonth(),
         };
         render();
     }
 
     function getPreviousMonth() {
-        var currentDate = new Date(current.year, current.month - 1);
+        var currentSelectedDate = new Date(current.year, current.month - 1);
+        if (options.history || (current.month <= currentDate.getMonth() && current.year == currentDate.getFullYear())) { return; } // Do not let them go backwards
         current = {
-            year: currentDate.getFullYear(),
-            month: currentDate.getMonth(),
+            year: currentSelectedDate.getFullYear(),
+            month: currentSelectedDate.getMonth(),
         };
         render();
     }
 
     function setDate(day) {
         var oldDateValue = targetEl.value;
-        var date = '%Y-%m-%d'
+        var date = options.format
             .replace('%d', ('0'+ day).slice(-2))
             .replace('%m', ('0' + (current.month + 1)).slice(-2))
             .replace('%Y', current.year);
@@ -234,6 +222,37 @@ function TinyPicker(query, options) {
         if (picker) { picker.remove(); }
     }
 
+    /*
+     * HELPERS
+     */
+
+    function extend(out) {
+        var args = arguments;
+        out = out || {};
+        for (var i = 1; i < args.length; i++) {
+            if (args[i]) {
+                for (var key in args[i]) {
+                    if (args[i].hasOwnProperty(key)) {
+                        out[key] = args[i][key];
+                    }
+                }
+            }
+        }
+        return out;
+    };
+
+    function parseDate(input) {
+        var parts = input.split('-');
+        return new Date(parts[0], parts[1]-1, parts[2]);
+    }
+
+    function createEl(type) {
+        return document.createElement(type);
+    }
+
+    function appendChild(parent, child){
+        parent.appendChild(child);
+    }
 }
 
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
