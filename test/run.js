bb99b315fda771ce67804b9fc92beb9564a80180
raymondborkowski <rborkows@umich.edu>
2018-03-02 15:21:02 -0500

    RB: Remove unused tests

--- test/run.js
+++
@@ -1,9 +0,0 @@
-var FeatherTestBrowser = require('feather-test-browser');
-
-// create a new test suite with your spec files
-var myTests = new FeatherTestBrowser({
-    specs: './specs'
-});
-
-// run your tests and get a link to run them again in any browser
-myTests.run(callback);
